d7cb4377da43e981d56ea3e51585c102de21288f
Bhavnit Patel <bhavnitrulez291@hotmail.com>
2016-12-30 13:31:26 +0000

    Implement logger and rotate over each user when making requests

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 var request = require('request');
+var winston = require('winston');
 
-var endpoint = "http://52.208.153.178";
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)({
+            "timestamp":true,
+            "colorize": true
+
+        }),
+        new (winston.transports.File)({ filename: 'D:/data_fetcher/log/fetcher.log' })
+    ]
+});
+
+
+var endpoint = "http://localhost";
 var port = ":3000";
-var bhav_token = "u1r_4oEFjcERitMMWaygT0HZjwblB7qMPAxB0JJSceafi4xZAqlZmNJuJ6MD-KTmnHGv14YiRz_SZK_iqV7QIVECdgRlo_GULMgGZS0EumxrKbZFiOmnmAPChBPDZ5JP";
-var dan_token = 'u1r_4oEFjcGXMeBUkvDdikHZjwblB7qMPAxB0JJSceafi4xZAqlZmKvq7UPSdXGUnHGv14YiRz_SZK_iqV7QIVECdgRlo_GULMgGZS0EumxrKbZFiOmnmAPChBPDZ5JP';
+var tokenList  = [
+    "u1r_4oEFjcERitMMWaygT0HZjwblB7qMPAxB0JJSceafi4xZAqlZmNJuJ6MD-KTmnHGv14YiRz_SZK_iqV7QIVECdgRlo_GULMgGZS0EumxrKbZFiOmnmAPChBPDZ5JP", // bhav token
+    'u1r_4oEFjcGXMeBUkvDdikHZjwblB7qMPAxB0JJSceafi4xZAqlZmKvq7UPSdXGUnHGv14YiRz_SZK_iqV7QIVECdgRlo_GULMgGZS0EumxrKbZFiOmnmAPChBPDZ5JP'
+]; // dan token
 
-var path = "/api/mood/updateMood";
-
-function postRequest(endpoint, port, path, jsonData,callback) {
-    console.log("postRequest(" + path + ")\n");
-    request.post(
-        endpoint + port + path,
-        jsonData,
-        function optionalCallback(err, httpResponse, body) {
-            if (httpResponse.statusCode != 200) {
-                return console.error('upload failed:', err);
-            } else {
-                console.log('Successful!  Server responded with: \n', JSON.stringify(body, null, 2));
-                return callback();
-            }
-        });
-
-}
-
+// list of endpoints to call
 var pathList = [
     "/settings/updateSettings",
     "/moves/updateMoves",
@@ -33,19 +31,58 @@ var pathList = [
     "/workouts/updateWorkouts"
 ];
 
+// function to send a POST request to the rest_app to update the DB
+function postRequest(endpoint, port, path, jsonData,callback) {
+    request.post(
+        endpoint + port + path,
+        jsonData,
+        function optionalCallback(err, httpResponse, body) {
+            if (httpResponse.statusCode != 200) {
+                return logger.error('request failed with :' + httpResponse.statusCode.toString(), err);
+            } else {
+                logger.debug('Successful!  Server responded with: \n', JSON.stringify(body, null, 2));
+                var json_res = JSON.parse(JSON.stringify(body, null, 2));
+                if (!json_res.Jawbone.error && !json_res.DynamoDB.error){
+                    logger.info("OK: postRequest(" + path + ")\n");
+                }
+
+                return callback();
+            }
+        });
+
+}
 
 
-var json = {json: {token: bhav_token}};
+// function to call POST requests for each Jawbone activity
+function sendRequests(token, callback) {
+    logger.debug("---Selecting token: " + token + "\n");
+    var json = {json: {token: token}};
+    var i = 0;
+
+
+    // callback function, executed once the previous post request has completed
+    var nextRequest = function () {
+        i++;
+        if (i < pathList.length) {
+            postRequest(endpoint, port, "/api" + pathList[i], json, nextRequest);
+        } else {
+            return callback();
+        }
+    };
+
+    // first post request, which triggers the rest via the callback
+    postRequest(endpoint, port, "/api" + pathList[i], json, nextRequest);
+}
+
+
+// loop through each user (token) and update DB
 var i = 0;
-
-
-
-var nextRequest = function() {
+var nextUser = function() {
     i++;
-    if (i < pathList.length) {
-        postRequest(endpoint, port, "/api" + pathList[i], json, nextRequest);
+    if (i < tokenList.length) {
+        sendRequests(tokenList[i], nextUser);
     }
 };
 
-
-postRequest(endpoint, port, "/api" + pathList[i], json, nextRequest);
+// first user request, which triggers the rest via the callback
+sendRequests(tokenList[i], nextUser);
