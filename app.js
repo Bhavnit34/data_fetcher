0b94c961276b64ada776313b8aaca11f0296df8b
Bhavnit Patel <bhavnitrulez291@hotmail.com>
2016-12-30 16:28:53 +0000

    Create log directory if it doesn't exist

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var winston = require('winston');
 var myArgs = require('optimist').argv,
     help = 'This would be a great place for real help information.';
@@ -12,6 +13,10 @@ if ((myArgs.h)||(myArgs.help)) {
 var logPath = __dirname + "/logs/";
 if (myArgs.logPath) {
     logPath = myArgs.logPath;
+}
+// create the log directory if it doesn't exist
+if (!fs.existsSync(logPath)){
+    fs.mkdirSync(logPath);
 }
 var logLevel = "info";
 if (myArgs.logLevel) {
